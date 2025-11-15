561d2f76c30fdb1617bfeebc6707a5152cdf0442
Vahelnir <vahelnir@gmail.com>
2025-11-15 19:00:49 +0100

    fix: improve circle generation in loader

--- src/components/big-loader.vue
+++ src/components/big-loader.vue
@@ -1,8 +1,4 @@
 <script setup lang="ts">
-import { roundDecimals } from "../utils/round-decimals";
-
-const CIRCLE_START_ANGLE = 180;
-
 withDefaults(
   defineProps<{
     activeCircles?: number;
@@ -11,21 +7,6 @@ withDefaults(
     activeCircles: 8,
   },
 );
-
-function circleStyle(index: number, total: number, radius: number) {
-  const angle = CIRCLE_START_ANGLE + ((index - 1) * 360) / total;
-  const rad = (angle * Math.PI) / 180;
-  const cosX = roundDecimals(Math.cos(rad), 2);
-  const sinY = roundDecimals(Math.sin(rad), 2);
-  const x = cosX * radius;
-  const y = sinY * radius;
-  return {
-    left: `calc(50% + ${x}px - 1.2rem)`,
-    top: `calc(50% + ${y}px - 1.2rem)`,
-    "--circle-translate-x": cosX,
-    "--circle-translate-y": sinY,
-  };
-}
 </script>
 
 <template>
@@ -33,21 +14,33 @@ function circleStyle(index: number, total: number, radius: number) {
     <div
       v-for="i in 8"
       :key="i"
-      class="absolute rounded-full h-10 w-10 transition-transform ease-in-out"
+      class="circle absolute rounded-full h-10 w-10 transition-transform ease-in-out"
       :class="{
         'bg-white/25': activeCircles < i,
         'bg-white circle-active': activeCircles >= i,
       }"
-      :style="circleStyle(i, 8, 10 * 16)"
+      :style="`--circle-index: ${i - 1}`"
     ></div>
   </div>
 </template>
 
 <style scoped>
+.circle {
+  /* Paramètres du cercle */
+  --circle-total: 8;
+  --circle-radius: 160px;
+  --circle-angle: calc(
+    180deg + (var(--circle-index) * 360deg / var(--circle-total))
+  );
+  left: calc(50% + cos(var(--circle-angle)) * var(--circle-radius) - 1.2rem);
+  top: calc(50% + sin(var(--circle-angle)) * var(--circle-radius) - 1.2rem);
+}
+
 .circle-active {
+  /* Décalage directionnel lors de l'activation */
   transform: translate(
-    calc(var(--circle-translate-x) * 2em),
-    calc(var(--circle-translate-y) * 2em)
+    calc(cos(var(--circle-angle)) * 2em),
+    calc(sin(var(--circle-angle)) * 2em)
   );
 }
 </style>
